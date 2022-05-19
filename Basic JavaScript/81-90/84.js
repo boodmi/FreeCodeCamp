37d1d3b420f379b198f116080ff466054866aef6
Boodmi <vano.boodmi@gmail.com>
2022-05-20 00:57:30 +0300

    Confirm 81-90

---
+++ Basic JavaScript/81-90/84.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    name: "AgaName",
+    legs: 1,
+    tails: 2,
+    friends: ["A", "B"]
+  
+    // Only change code above this line
+  };
