46da5f42bf837e7b5ddbae8b9b746649b160305a
Boodmi <vano.boodmi@gmail.com>
2022-05-20 19:28:28 +0300

    Basic Java 91-100

---
+++ Basic JavaScript/91-100/93.js
@@ -0,0 +1,22 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Billy Miligam",
+      "title": "Agegey",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "LD"
+      ]
+    }
+  ];
